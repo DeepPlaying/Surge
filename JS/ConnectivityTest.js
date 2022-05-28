35a9bfad11b035cc6eae945ef135f1cd0efecc9c
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 18:02:40 +0800

    Update ConnectivityTest.js

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -3,7 +3,7 @@ const REQUEST_HEADERS = {
     'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
   'Accept-Language': 'en',
 }
-
+setInterval("ping++",1);
 ;(async () => {
   let panel_result = {
     title: '网络连通性测试',
@@ -28,34 +28,18 @@ async function check_youtube() {
         url: 'https://www.youtube.com',
         headers: REQUEST_HEADERS,
       }
-      $httpClient.get(option, function (error, response, data) {
-        if (error != null || response.status !== 200) {
-          reject('Error')
-          return
-        }
-        
-        let delay = ''
-        region = '${response.status}ms$'
-
-
-        resolve(region)
+      ping = 0
+      $httpClient.get(option, function (error, response, data) {      
       })
+      let delay = ''
+      delay = '${ping}ms$'
     })
   }
 
   let youtube_check_result = ''
 
-  await inner_check()
-    .then((code) => {
-      if (code === 'Not Available') {
-        youtube_check_result += '油管未解锁'
-      } else {
-        youtube_check_result += '油管解锁：' + code.toUpperCase()
-      }
-    })
-    .catch((error) => {
-      youtube_check_result += '检测失败'
-    })
+  youtube_check_result += '百度：' + delay
+
 
   return youtube_check_result
 }

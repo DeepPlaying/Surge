c03a1205aa617764598b9bcef82718dda7dc9455
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 21:56:32 +0800

    Update ConnectivityTest.js

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -56,9 +56,7 @@ if(net=="en0") {
 
   $done({
       title:"流量统计 | "+netType,
-      content:`流量 ➟ ${upload} | ${download}\n`+
-      `速度 ➟ ${outCurrentSpeed} | ${inCurrentSpeed}\n` +
-		`峰值 ➟ ${outMaxSpeed} | ${inMaxSpeed}`,
+      content: network,
 		icon: params.icon,
 		  "icon-color":params.color
     });

e9c5153d335a0c07dc01bec8a3d8fc0af75b28cf
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 22:24:47 +0800

    Update sub_info.js

--- JS/sub_info.js
+++ JS/sub_info.js
@@ -14,7 +14,7 @@ let args = getArgs();
   let total = info.total;
   let expire = args.expire || info.expire;
   
-  let content = [`Used：${bytesToSize(used)}\nFree：${bytesToSize(total-used)}`];
+  let content = [`Used：${bytesToSize(used)} | Free：${bytesToSize(total-used)}`];
 
 
   let now = new Date();
@@ -105,7 +105,6 @@ function bytesToSize(bytes) {
   let k = 1024;
   sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
   let i = Math.floor(Math.log(bytes) / Math.log(k));
-  if (i==4) i=i-1;
   return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
 }
 

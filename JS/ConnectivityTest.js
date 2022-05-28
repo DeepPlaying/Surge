c88350c268549833faceae1393efe2f9d9f100aa
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 18:18:30 +0800

    Update ConnectivityTest.js

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -52,7 +52,7 @@ async function test_youtube() {
       }
     })
     .catch((error) => {
-      youtube_check_result += '检测失败'
+      youtube_check_result += '油管解锁：' + delay
     })
   youtube_check_result += '油管解锁：' + delay
   return youtube_check_result

2ec1f5a2b0c7fc8ca2561aea3d79688e2c287ada
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 23:52:30 +0800

    Update ConnectivityTest.js

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -1,3 +1,8 @@
+const REQUEST_HEADERS = {
+  'User-Agent':
+    'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+  'Accept-Language': 'en',
+};
 !(async () => {
     let panel = { title: "Flush DNS" };
     if (typeof $argument != "undefined") {
@@ -7,8 +12,10 @@
         if (arg.color) panel["icon-color"] = arg.color;
         if (arg.server == "false") showServer = false;
     }
-    let test = (await httpAPI2("/v1")) ;
-    console.log(test.results);
+    var ping = 0;
+    setTimeout(function(){ ping++; }, 1);
+    test();
+    console.log(ping);
     $done();
 })();
 
@@ -26,4 +33,39 @@ function httpAPI2(path = "", method = "GET", body = null) {
             resolve(result);
         });
     });
+}
+
+function test() {
+  return new Promise((resolve, reject) => {
+    let option = {
+      url: BASE_URL,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36',
+        'Accept-Language': 'en',
+      },
+    }
+    $httpClient.get(option, function (error, response, data) {
+      if (error != null || response.status !== 200) {
+        reject('Error')
+        return
+      }
+
+      if (data.indexOf('Premium is not available in your country') !== -1) {
+        reject('Not Available')
+        return
+      }
+
+      let region = ''
+      let re = new RegExp('"countryCode":"(.*?)"', 'gm')
+      let result = re.exec(data)
+      if (result != null && result.length === 2) {
+        region = result[1]
+      } else if (data.indexOf('www.google.cn') !== -1) {
+        region = 'CN'
+      } else {
+        region = 'US'
+      }
+      resolve(region.toUpperCase())
+    })
+  })
 }

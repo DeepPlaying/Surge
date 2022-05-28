f6fe2627b1b4f2af2d1f712315356087c386b2c6
DeepPlaying <96329688+DeepPlaying@users.noreply.github.com>
2022-05-28 22:27:17 +0800

    Update ConnectivityTest.js

--- JS/ConnectivityTest.js
+++ JS/ConnectivityTest.js
@@ -1,111 +1,30 @@
-let params = getParams($argument)
-
-;(async () => {
-
-let traffic = (await httpAPI("/v1/traffic"))
-let interface = traffic.interface
-
-/* 获取所有网络界面 */
-let allNet = [];
-for (var key in interface){
-   allNet.push(key)
+!(async () => {
+    let panel = { title: "Flush DNS" },
+        showServer = true,
+        dnsCache;
+    if (typeof $argument != "undefined") {
+        let arg = Object.fromEntries($argument.split("&").map((item) => item.split("=")));
+        if (arg.title) panel.title = arg.title;
+        if (arg.icon) panel.icon = arg.icon;
+        if (arg.color) panel["icon-color"] = arg.color;
+        if (arg.server == "false") showServer = false;
     }
+    if (showServer) {
+        dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
+        dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
+    }
+    if ($trigger == "button") await httpAPI("/v1/dns/flush");
+    let traffic = (await httpAPI("/v1/traffic"));
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    panel.content = `Delay: ${delay}ms`;
+    console.log(traffic);
+    $done(panel);
+})();
 
-if(allNet.includes("lo0")==true){
-del(allNet,"lo0")
-}
-
-let net;
-let index;
-if( $persistentStore.read("NETWORK")==null||allNet.includes($persistentStore.read("NETWORK"))==false){
-	index = 0
-	}else{
-	net = $persistentStore.read("NETWORK")
-	for(let i = 0;i < allNet.length; ++i) {
-		if(net==allNet[i]){
-		index=i
-		}
-	}
-}
-
-/* 手动执行时切换网络界面 */
-if($trigger == "button"){
-	if(allNet.length>1) index += 1
-	if(index>=allNet.length) index = 0;
-	$persistentStore.write(allNet[index],"NETWORK")
-};
-
-net = allNet[index]
-let network = interface[net]
-
-let outCurrentSpeed = speedTransform(network.outCurrentSpeed) //上传速度
-let outMaxSpeed = speedTransform(network.outMaxSpeed) //最大上传速度
-let download = bytesToSize(network.in) //下载流量
-let upload = bytesToSize(network.out) //上传流量
-let inMaxSpeed = speedTransform(network.inMaxSpeed) //最大下载速度
-let inCurrentSpeed = speedTransform(network.inCurrentSpeed) //下载速度
-
-/* 判断网络类型 */
-let netType;
-if(net=="en0") {
-	netType = "WiFi"
-	}else{
-	netType = "Cellular"
-	}
-
-
-  $done({
-      title:"流量统计 | "+netType,
-      content: traffic,
-		icon: params.icon,
-		  "icon-color":params.color
-    });
-
-})()
-
-function bytesToSize(bytes) {
-  if (bytes === 0) return "0B";
-  let k = 1024;
-  sizes = ["B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"];
-  let i = Math.floor(Math.log(bytes) / Math.log(k));
-  return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
-}
-
-function speedTransform(bytes) {
-  if (bytes === 0) return "0B/s";
-  let k = 1024;
-  sizes = ["B/s", "KB/s", "MB/s", "GB/s", "TB/s", "PB/s", "EB/s", "ZB/s", "YB/s"];
-  let i = Math.floor(Math.log(bytes) / Math.log(k));
-  return (bytes / Math.pow(k, i)).toFixed(2) + " " + sizes[i];
-}
-
-
-function httpAPI(path = "", method = "GET", body = null) {
+function httpAPI(path = "", method = "POST", body = null) {
     return new Promise((resolve) => {
         $httpAPI(method, path, body, (result) => {
-			
             resolve(result);
         });
     });
-};
-
-
-function getParams(param) {
-  return Object.fromEntries(
-    $argument
-      .split("&")
-      .map((item) => item.split("="))
-      .map(([k, v]) => [k, decodeURIComponent(v)])
-  );
 }
-
-function del(arr,num) {
-			var l=arr.length;
-		    for (var i = 0; i < l; i++) {
-			  	if (arr[0]!==num) { 
-			  		arr.push(arr[0]);
-			  	}
-			  	arr.shift(arr[0]);
-		    }
-		    return arr;
-		}
